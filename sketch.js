d8724a38ac0d4d3f488dcb49246d16357a5c2a20
NICOLASKEVSIL3S2024 <00001096108768sp@al.educacao.sp.gov.br>
2024-08-17 11:57:40 -0300

    modificado

--- sketch.js
+++ sketch.js
@@ -1,29 +1,30 @@
-let palavra;
-function setup() {
-  createCanvas(400, 400);
-  palavra = palavraAleatoria();
-}
-function palavraAleatoria(){
-  let palavras = ["Caminhante", "Caminho", "Caminha"];
-  return random(palavras);
+let cor;
+let circuloX;
+let circuloY;
+
+function setup () {
+  createCanvas (400, 400);;
+  background ("brown")
+  cor = color (random (0,255),random(0.,255),random(0,255));
+  circuloX = [0,0,0];
+  circuloY= [random(height)],[random(height)],[random(height)];
 }
 
-function inicializaCores(){
-  background("orange");
-  fill("black");
-  textSize(64);
-  textAlign(CENTER, CENTER);
+function draw(){
+  fill (cor)
+  for(let contador in circuloX){
+circle(circuloX[contador],circuloY[contador], 50)
+  circuloX[contador]+=random(0,3);
+  circuloY[contador]+=random(-3,3);
+
+  if(circuloX[contador] >= width){
+    circuloX[contador]= 0;
+    circuloY[contador]= random(height);
+  }
+  if(mouseIsPressed){
+    cor = color (random (0,255),random (0,255),random (0,255),random (0,100));
+    }
+  }
 }
 
-function palavraParcial (minimo, maximo){
-  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
-  let parcial = palavra.substring(0, quantidade);
-  return parcial;
-}
-
-function draw() {
-  inicializaCores();
-  let texto = palavraParcial(0, width);
-  text(texto, 200, 200);
   
-}
